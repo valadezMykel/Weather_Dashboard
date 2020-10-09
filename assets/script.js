e312f4f8513dd0a6ad9fdefb1763c91fa32edaf0
Mykel Valadez <valadezmykel@gmail.com>
2020-10-08 17:15:45 -0700

    to do list items loads on search and prevents unidentified cities

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
+var isCity = true;
+var cityName = "";
+let isNew = false;
+
 // when the city search button is clicked
 // appends the city to the city container div
-$("#searchBtn").on("click", () =>{;
+$("#searchBtn").on("click", () =>{
     let cityVal = $("input").val();
-    cityListPopulator(cityVal);
+    isNew = true;
+    whoYouGonnaCall(cityVal);
     // clears the input text
     $("input").val("");
 });
@@ -13,37 +18,46 @@ $("#searchBtn").on("click", () =>{;
 $("#city-list").on("click", (event) =>{
     // get the event target and the city name from the target
     if(event.target.matches("button")){
-        let cityName = event.target.textContent;
-        $(".fiveDayForecast").remove();
-        whoYouGonnaCall(cityName);
+        let cityVal = event.target.textContent;
+        isNew = false;
+        whoYouGonnaCall(cityVal);
         
     }
 });
 
+
 function cityListPopulator(cityVal){
-    let cityEL = $("<button>");
     if(cityVal !== '' ){
+        let cityEL = $("<button>");
         cityEL.text(cityVal);
         // cityEL.attr("data", "city")
-        cityEL.attr("class", "btn btn-white btn-block")
+        cityEL.attr("class", "btn btn-block cityListBtn");
         $("#city-list").prepend(cityEL);
         localStorage.setItem("lastCity", cityVal)
     }
 }
 
-function whoYouGonnaCall (cityName){
+function whoYouGonnaCall (cityVal){
     // send an ajaxs call with the city name for the #btnForecast
     // 1 get api key
     let apiKey = "46c83042b2cdf276d685079cb38dbf65";
     // 2 get query url
-    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=imperial&appid="+apiKey;
+    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q="+cityVal+"&units=imperial&appid="+apiKey;
     // 3 send call and then function
     $.ajax({
         url: apiUrl,
         method: "GET"
-    }).then((response) =>{
-        console.log(response);
-        $("#cityNameDate").text((response.name)+" "+moment().format('L'));
+    })
+    .then((response) =>{
+        // delete old boxes 
+        $(".fiveDayForecast").remove();
+        let cityName = response.name;
+        // if the call from the search button then make a new button in the city list
+        if(isNew){
+            cityListPopulator(cityName);
+        };
+        // create a city list button
+        $("#cityNameDate").text((cityName)+" "+moment().format('L'));
         $("#cityNameDate").append($("<img>").attr("src",  "http://openweathermap.org/img/wn/"+ response.weather[0].icon+"@2x.png" ));
         $("#temp").html("Temperature: "+(response.main.temp)+ '9&#176'+"F");
         $("#humidity").text(response.main.humidity + "%");
@@ -55,9 +69,9 @@ function whoYouGonnaCall (cityName){
             url: "http://api.openweathermap.org/data/2.5/uvi?lat="+lat+"&lon="+lon+"&appid="+apiKey,
             method: "GET"
         }).then((response) =>{
-            console.log(response);
             let uvi = response.value;
             $("#uv").text("UV Index: ");
+            // colors the uvi based on its value
             if(uvi < 3){
                 $("#uv").append(
                     $("<div>").attr("class", "low").text(uvi)
@@ -78,13 +92,12 @@ function whoYouGonnaCall (cityName){
                     $("<div>").attr("class", "veryHigh").text(uvi)
                 )
             }
-        })
+        });
         // get a 5 day forecast from a today and next 7 days forecast 
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&exclude=current,minutely,hourly,alerts&appid="+apiKey,
             method: "GET"
         }).then((response) =>{
-            console.log(response);
             let dailyForecastArr = response.daily;
             for(let i = 1; i < 6; i++){
                 let forecastDiv = $("<div>").attr("class", "fiveDayForecast");
@@ -97,10 +110,18 @@ function whoYouGonnaCall (cityName){
 
             };
         });
+    }).catch(function(e) {
+        alert("not a valid city");
+        isCity = false;
+
     });
 };
 
 // when the page loads populate with data from the last searched city
-console.log(localStorage.getItem("lastCity"));
-cityListPopulator(localStorage.getItem("lastCity"));
 whoYouGonnaCall(localStorage.getItem("lastCity"));
+cityListPopulator(localStorage.getItem("lastCity"));
+
+
+// to do list: 
+// make it responsive and mobile friendly
+// update the css and improve appearance
