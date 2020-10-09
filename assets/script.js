a323981d34c4362ba8323373f102a9252533a7f7
Mykel Valadez <valadezmykel@gmail.com>
2020-10-08 17:34:01 -0700

    refactored the code to be cleaner and added a title

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,12 @@
-var isCity = true;
-var cityName = "";
 let isNew = false;
 
 // when the city search button is clicked
-// appends the city to the city container div
+// appends the city to the city container div and displays weather info
 $("#searchBtn").on("click", () =>{
     let cityVal = $("input").val();
+    // sets up the whoYouGonnaCall function to add a button to the cityButtonList
     isNew = true;
+    // displays weather
     whoYouGonnaCall(cityVal);
     // clears the input text
     $("input").val("");
@@ -14,27 +14,27 @@ $("#searchBtn").on("click", () =>{
 
 
 // when a city button is clicked 
-// it populates the forecast card
+// it populates the forecasts
 $("#city-list").on("click", (event) =>{
     // get the event target and the city name from the target
     if(event.target.matches("button")){
         let cityVal = event.target.textContent;
+        // tells whoYouGonnaCall not to make a new button
         isNew = false;
         whoYouGonnaCall(cityVal);
         
     }
 });
 
-
+// adds buttons to the cityList div
 function cityListPopulator(cityVal){
-    if(cityVal !== '' ){
-        let cityEL = $("<button>");
-        cityEL.text(cityVal);
-        // cityEL.attr("data", "city")
-        cityEL.attr("class", "btn btn-block cityListBtn");
-        $("#city-list").prepend(cityEL);
-        localStorage.setItem("lastCity", cityVal)
-    }
+    // creates and appends
+    let cityEL = $("<button>");
+    cityEL.text(cityVal);
+    cityEL.attr("class", "btn btn-block cityListBtn");
+    $("#city-list").prepend(cityEL);
+    // saves the last city to be added to the city-list div in local storage
+    localStorage.setItem("lastCity", cityVal)
 }
 
 function whoYouGonnaCall (cityVal){
@@ -49,19 +49,23 @@ function whoYouGonnaCall (cityVal){
         method: "GET"
     })
     .then((response) =>{
-        // delete old boxes 
-        $(".fiveDayForecast").remove();
         let cityName = response.name;
+
         // if the call from the search button then make a new button in the city list
         if(isNew){
             cityListPopulator(cityName);
         };
-        // create a city list button
+
+        // delete old boxes 
+        $(".fiveDayForecast").remove();
+
+        // begin populating button forecast div
         $("#cityNameDate").text((cityName)+" "+moment().format('L'));
         $("#cityNameDate").append($("<img>").attr("src",  "http://openweathermap.org/img/wn/"+ response.weather[0].icon+"@2x.png" ));
         $("#temp").html("Temperature: "+(response.main.temp)+ '9&#176'+"F");
         $("#humidity").text(response.main.humidity + "%");
         $("#windSpeed").text(response.wind.speed + " MPH");
+
         // looks like I need to make another call for uvi
         let lon = response.coord.lon;
         let lat = response.coord.lat;
@@ -75,33 +79,38 @@ function whoYouGonnaCall (cityVal){
             if(uvi < 3){
                 $("#uv").append(
                     $("<div>").attr("class", "low").text(uvi)
-                )
+                );
             }
             else if(uvi < 6){
                 $("#uv").append(
                     $("<div>").attr("class", "moderate").text(uvi)
-                )
+                );
             }
             else if(uvi < 8){
                 $("#uv").append(
                     $("<div>").attr("class", "high").text(uvi)
-                )
+                );
             }
             else{
                 $("#uv").append(
                     $("<div>").attr("class", "veryHigh").text(uvi)
-                )
-            }
+                );
+            };
         });
-        // get a 5 day forecast from a today and next 7 days forecast 
+
+        // get a 5 day forecast from a today and the next 7 days forecast 
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&units=imperial&exclude=current,minutely,hourly,alerts&appid="+apiKey,
             method: "GET"
         }).then((response) =>{
             let dailyForecastArr = response.daily;
+            // loops through the next day to the next five days
             for(let i = 1; i < 6; i++){
+                // creates a new div for each day 
                 let forecastDiv = $("<div>").attr("class", "fiveDayForecast");
-                let date = String(moment().add(i, 'day').format('L')); 
+                let date = String(moment().add(i, 'day').format('L'));
+                
+                // adds the weather info to the div
                 forecastDiv.prepend($("<p>").text(date));
                 forecastDiv.append($("<img>").attr("src", "http://openweathermap.org/img/wn/"+dailyForecastArr[i].weather[0].icon+"@2x.png"));
                 forecastDiv.append($("<p>").html("Temp: "+dailyForecastArr[i].temp.day+'9&#176'+"F"));
@@ -110,10 +119,9 @@ function whoYouGonnaCall (cityVal){
 
             };
         });
-    }).catch(function(e) {
-        alert("not a valid city");
-        isCity = false;
-
+    // this will run if there is an error that occurs when calling the info
+    }).catch(() => {
+        alert("Not a valid city, Please check the spelling and try again.");
     });
 };
 
@@ -125,3 +133,4 @@ cityListPopulator(localStorage.getItem("lastCity"));
 // to do list: 
 // make it responsive and mobile friendly
 // update the css and improve appearance
+// round the degrees to 2 decimals
